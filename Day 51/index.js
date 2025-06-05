4087be732dde3de7b9db40e1e97f6a2a4c89e228
Hardelz <135560555+Hardelzs@users.noreply.github.com>
2025-06-05 21:01:26 +0100

    Refactor slider implementation: clean up CSS selectors and improve JavaScript formatting

--- Day 51/index.js
+++ Day 51/index.js
@@ -1,10 +1,14 @@
-let slider =document.querySelector('.slider')
-let nextBtn =document.getElementById('next')
-let prevBtn =document.getElementById('prev')
+let slider = document.querySelector('.slider')
+let nextBtn = document.getElementById('next')
+let prevBtn = document.getElementById('prev')
 
 nextBtn.onclick = () => {
    slider.append( slider.querySelector('img:first-child'));
+   console.log('slider', slider);
+   
 }
 prevBtn.onclick = () => {
    slider.prepend( slider.querySelector('img:last-child'));
+   console.log('slider', slider);
+   
 }
